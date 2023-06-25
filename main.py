bc90aad72c41fa0d7814378e6a1459ffb05599c8
Charan Sandha <cpksandha@gmail.com>
2023-06-24 18:38:36 -0700

    Commit this

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
-    print(sub2)
+    sub1= original_str[0:6]
+    sub2= original_str[7:19]
     print(sub1)
+    print(sub2)
+    merged_str= sub2 + " " + sub1
     print(merged_str)
 
     #########################################
     # Do not delete the return statement
-    return sub1, sub2, merged_str
+    # return sub1, sub2, merged_str
 
 
 if __name__ == '__main__':
